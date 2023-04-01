583b79af4850ae33b2a114a21f26db4e61142cb1
zhanghairui <herry0412@163.com>
2023-04-01 14:20:49 +0800

    数组中心位置

--- 数组/查找数组中心位置.go
+++ 数组/查找数组中心位置.go
@@ -1,1 +1,48 @@
-package 数组
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+)
+
+func main() {
+	scanner := bufio.NewScanner(os.Stdin)
+
+	for scanner.Scan() {
+		strs := strings.Split(scanner.Text(), " ")
+
+		var nums []int
+		for _, v := range strs {
+			n, _ := strconv.Atoi(v)
+			nums = append(nums, n)
+		}
+
+		var left = 1
+		var right = 1
+		var flag = false
+		for _, v := range nums {
+			right *= v
+		}
+
+		for i, v := range nums {
+			if i > 0 {
+				left *= nums[i-1]
+			}
+			right = right / v
+			if left == right {
+				fmt.Println(i)
+				flag = true
+				break
+			}
+
+		}
+
+		if !flag {
+			fmt.Println(-1)
+		}
+	}
+
+}
